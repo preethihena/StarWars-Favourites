df9ad83af8d423032dc238603b496e39f3ee63f0
preethihena <preethihena.d16@iiits.in>
2021-12-18 17:42:56 +0530

    Adds functionality for:
    - get single planet and movie details
    
    - get all planets and movies along with page number
    
    - search for planets and movies using name/title

--- favourites/urls.py
+++ favourites/urls.py
@@ -5,6 +5,9 @@ from favourites import views
 
 urlpatterns = [
     path('api/planets/<int:id>', views.SinglePlanetView.as_view()),
+    path('api/planets/', views.AllPlanetView.as_view()),
+    path('api/movies/<int:id>', views.SingleMovieView.as_view()),
+    path('api/movies/', views.AllMoviesView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- favourites/views.py
+++ favourites/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from favourites.models import Planet
+from favourites.models import Movie, Planet
 from favourites.serializers import ResourceSerializer
 from favourites.swapiutils import SwapiService
 
@@ -14,6 +14,17 @@ def create_planet_response(serializer_data, swapi_data):
     response['name'] = swapi_data['name']
     response['url'] = swapi_data['url']
     response['created'] = swapi_data['created']
+    return response
+
+def create_movie_response(serializer_data, swapi_data):
+    response = {}
+    response['custom_name'] = serializer_data['custom_name']
+    response['is_favourite'] = serializer_data['is_favourite']
+    response['updated'] = serializer_data['last_updated']
+    response['title'] = swapi_data['title']
+    response['url'] = swapi_data['url']
+    response['created'] = swapi_data['created']
+    response['release_date'] = swapi_data['release_date']
     return response
 
 
@@ -32,3 +43,97 @@ class SinglePlanetView(APIView):
             response = create_planet_response(serializer.data, swapi_data)
             return JsonResponse(status=201, data=response)
         return JsonResponse(status=400, data="wrong parameters")
+    
+    def get(self, request, **kwargs):
+        swapi_service = SwapiService('planets')
+        id = kwargs["id"]
+        swapi_data = swapi_service.get_resource_by_id(id)
+        if 'detail' in swapi_data.keys():
+            return JsonResponse(status=200, data=swapi_data)
+        planet_object, created = Planet.objects.get_or_create(id = id)
+        serializer = ResourceSerializer(planet_object)
+        response = create_planet_response(serializer.data, swapi_data)
+        return JsonResponse(data=response)
+
+
+class AllPlanetView(APIView):
+    def get(self, request):
+        swapi_service = SwapiService('planets')
+        page = 1
+        if "page" in request.GET.keys():
+            page = request.GET.get("page")
+        if "search" not in request.GET.keys():
+            swapi_data = swapi_service.get_all_resources(page)
+        else:
+            search_value = request.GET.get("search")
+            swapi_data = swapi_service.search(search_value, page)
+        if "detail" in swapi_data.keys():
+            return JsonResponse(status=200, data=swapi_data)
+        if len(swapi_data["results"]) == 0:
+            return JsonResponse(status=200, data=swapi_data)
+        response = {}
+        response["count"] = swapi_data["count"]
+        response["next"] = swapi_data["next"]
+        response["previous"] = swapi_data["previous"]
+        response["results"] = []
+        for planet in swapi_data['results']:
+            id = int(planet['url'].split('/')[-2])
+            planet_object, created = Planet.objects.get_or_create(id = id)
+            serializer = ResourceSerializer(planet_object)
+            response["results"].append(create_planet_response(serializer.data, planet))
+        return JsonResponse(data=response)
+            
+
+class SingleMovieView(APIView):
+    def post(self, request, **kwargs):
+        swapi_service = SwapiService('films')
+        id = kwargs["id"]
+        swapi_data = swapi_service.get_resource_by_id(id)
+        if 'detail' in swapi_data.keys():
+            return JsonResponse(status=201, data=swapi_data)
+        movie_object, created = Movie.objects.get_or_create(id = id)
+        serializer = ResourceSerializer(movie_object, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            response = create_movie_response(serializer.data, swapi_data)
+            return JsonResponse(status=201, data=response)
+        return JsonResponse(status=400, data="wrong parameters")
+    
+    def get(self, request, **kwargs):
+        swapi_service = SwapiService('films')
+        id = kwargs["id"]
+        swapi_data = swapi_service.get_resource_by_id(id)
+        if 'detail' in swapi_data.keys():
+            return JsonResponse(status=200, data=swapi_data)
+        movie_object, created = Movie.objects.get_or_create(id = id)
+        serializer = ResourceSerializer(movie_object)
+        response = create_movie_response(serializer.data, swapi_data)
+        return JsonResponse(data=response)
+
+
+class AllMoviesView(APIView):
+    def get(self, request):
+        swapi_service = SwapiService('films')
+        page = 1
+        if "page" in request.GET.keys():
+            page = request.GET.get("page")
+        if "search" not in request.GET.keys():
+            swapi_data = swapi_service.get_all_resources(page)
+        else:
+            search_value = request.GET.get("search")
+            swapi_data = swapi_service.search(search_value, page)
+        if "detail" in swapi_data.keys():
+            return JsonResponse(status=200, data=swapi_data)
+        if len(swapi_data["results"]) == 0:
+            return JsonResponse(status=200, data=swapi_data)
+        response = {}
+        response["count"] = swapi_data["count"]
+        response["next"] = swapi_data["next"]
+        response["previous"] = swapi_data["previous"]
+        response["results"] = []
+        for movie in swapi_data['results']:
+            id = int(movie['url'].split('/')[-2])
+            movie_object, created = Movie.objects.get_or_create(id = id)
+            serializer = ResourceSerializer(movie_object)
+            response["results"].append(create_movie_response(serializer.data, movie))
+        return JsonResponse(data=response)
